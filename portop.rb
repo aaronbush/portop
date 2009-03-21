02adab20daa6fab65113f507b06958a0df772874
Aaron S. Bush <asb.bush@gmail.com>
2009-03-21 16:41:53 -0400

    much cleaner storage of port info now.

--- portop.rb
+++ portop.rb
@@ -2,64 +2,70 @@
 
 require 'rubygems'
 require 'snmp'
+require 'erb'
+
 include SNMP
 
 MAX_ROWS = 4
-previfInOctets = 0
-previfOutOctets = 0
+HDR = "Description    InRate    OutRate    AdminState/OperState    Speed        Last Change"
+SEP = "===================================================================================="
 
-portInfo = {}
+
+ports = Array.new
+PortInfo = Struct.new(:hostname, :ifDescr, :ifAdminStatus, :ifOperStatus, :ifSpeed, :ifLastChange, :ifInOctets, :ifInOctetsDelta, :ifOutOctets, :ifOutOctetsDelta)
+
 host = "coswm01"
-first_run = TRUE
+delay = 2
+
+# ----------------------
+# show the current stats
+# ----------------------
+puts HDR
+puts SEP
 
 while true do
-Manager.open(:Host => host) do |manager|
-	response = manager.get_bulk(0, MAX_ROWS, ["ifDescr", "ifAdminStatus", "ifOperStatus", "ifSpeed", "ifLastChange", "ifInOctets", "ifOutOctets"])
+	Manager.open(:Host => host) do |manager|
+		response = manager.get_bulk(0, MAX_ROWS, ["ifDescr", "ifAdminStatus", "ifOperStatus", "ifSpeed", "ifLastChange", "ifInOctets", "ifOutOctets"])
 	
-	list = response.varbind_list
-	portInfo[host] = {}
-	portInfo[host]["vb_list"] = list
-	portInfo[host]["prev_vb_list"] = list if first_run
-	portInfo[host]["vb_list"] = list
+		list = response.varbind_list
+	
+		until list.empty?
+			ifDescr = list.shift.value.to_s
+			ifAdminStatus = list.shift.value.to_i
+			ifOperStatus = list.shift.value.to_i
+			ifSpeed = list.shift.value.to_f
+			ifLastChange = list.shift
+			ifInOctets = list.shift.value.to_f
+			ifOutOctets = list.shift.value.to_f
 
-	#puts portInfo[host]["vb_list"].length
-	#puts portInfo[host]["vb_list"][1]
-	#puts portInfo[host]["vb_list"][1].name
-	#puts portInfo[host]["vb_list"][1].value
+			# are we updating an existing element?
+			if pe = ports.find { |port| port.hostname == host && port.ifDescr == ifDescr }
+				p pe
+				pe.ifInOctetsDelta = (ifInOctets - pe.ifInOctets)
+				pe.ifInOctets = ifInOctets 
+				pe.ifOutOctetsDelta = (ifOutOctets - pe.ifOutOctets)
+				pe.ifOutOctets = ifOutOctets 
+			else
+				ports.push(
+					PortInfo.new(
+						host,
+						ifDescr,
+						ifAdminStatus,
+						ifOperStatus,
+						ifSpeed,
+						ifLastChange,
+						ifInOctets,
+						0, # ifInOctetsDelta
+						ifOutOctets,
+						0 # ifOutOctetsDelta
+					)
+				)
+				#p ports
+			end
 
-	# ----------------------
-	# show the current stats
-	# ----------------------
+		end # until done with result set
 
-	puts "Description    InRate    OutRate    AdminState/OperState    Speed        Last Change"
-	puts "===================================================================================="
+	end # Manager.open
 
-	until list.empty?
-		ifDescr = list.shift
-		ifAdminStatus = list.shift
-		ifOperStatus = list.shift
-		ifSpeed = list.shift
-		ifLastChange = list.shift
-		if first_run
-			ifInOctets = "?"
-			ifOutOctets = "?"
-			ifInRate = "N/A"
-			ifOutRate = "N/A"
-			list.shift
-			list.shift
-		else
-			ifInOctets = list.shift
-			ifOutOctets = list.shift
-			ifInRate = ifInOctets.value.to_i - previfInOctets
-			ifOutRate = ifOutOctets.value.to_i - previfOutOctets
-		end
-		#puts "#{ifDescr.value}    #{ifAdminStatus.value}/#{ifOperStatus.value}	#{ifSpeed.value}	#{ifLastChange.value} AGO"
-
-
-		puts "#{ifDescr.value}   #{ifInRate}    #{ifOutRate}    #{ifAdminStatus.value}/#{ifOperStatus.value} #{ifSpeed.value}  #{ifLastChange.value}"
-
-		first_run = FALSE
-	end
-end
-
-end # keep it going
+	sleep delay
+end # while to keep it going
